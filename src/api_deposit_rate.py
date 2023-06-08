2a52063f14c314fc6b7d3f1f93b49a061fd02c08
Poxios <poxios0310@gmail.com>
2023-06-09 01:03:26 +0900

    fixed deposit rate

--- src/api_deposit_rate.py
+++ src/api_deposit_rate.py
@@ -1,5 +1,4 @@
 import json
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
@@ -9,10 +8,7 @@ options.add_argument('headless')
 options.add_argument('window-size=1920x1080')
 options.add_argument("disable-gpu")
 options.add_argument('log-level=3')
-
-c = DesiredCapabilities.CHROME
-# To bypass page loading
-c["pageLoadStrategy"] = "eager"
+options.page_load_strategy = 'eager'
 
 
 def get_running_env_from_secret():
@@ -27,30 +23,34 @@ def get_highest_deposit_rate():
         driver = None
         RUNNING_ENV = get_running_env_from_secret()
         if RUNNING_ENV == 'Windows':
-            driver = webdriver.Chrome(
-                'chromedriver', options=options, desired_capabilities=c)
+            driver = webdriver.Chrome('chromedriver', options=options)
         else:
-            driver = webdriver.Chrome(options=options, desired_capabilities=c)
+            driver = webdriver.Chrome(options=options)
 
-        driver.get('https://finlife.fss.or.kr/deposit/selectDeposit.do')
+        driver.get('https://finlife.fss.or.kr/finlife/svings/fdrmDpst/list.do?menuNo=700002')
         sleep(1)
-        # click '복리'
+        # # click '복리'
         driver.find_element(
-            By.XPATH, '/html/body/div[2]/section[1]/div[2]/div[1]/div[3]/ul/li[3]/button').click()
+            By.XPATH, '/html/body/div[2]/div[2]/main/div[2]/div[1]/div[3]/dl/dd/div/div[3]').click()
 
-        # click search
+        # # click search
         driver.find_element(
-            By.XPATH, '/html/body/div[2]/section[1]/div[2]/div[1]/button').click()
+            By.XPATH, '/html/body/div[2]/div[2]/main/div[2]/div[1]/div[8]/div/button[1]').click()
 
         sleep(1)
 
-        highest_deposit_center = driver.find_element(
-            By.XPATH, '/html/body/div[2]/section[1]/div[2]/div[2]/div[1]/div[4]/div[2]/table/tbody/tr[1]/td[2]').text
-        highest_deposit_rate = driver.find_element(
-            By.XPATH, '/html/body/div[2]/section[1]/div[2]/div[2]/div[1]/div[4]/div[2]/table/tbody/tr[1]/td[4]').text
+        highest_deposit_center = driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/main/div[2]/div[8]/table/tbody/tr[1]/td[3]').text
+        
+        highest_deposit_rate = driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/main/div[2]/div[8]/table/tbody/tr[1]/td[4]').text
 
         driver.quit()
         return f'{highest_deposit_center}: {highest_deposit_rate}\n'
 
     except Exception as e:
-        return f'[DEPOSIT_ERROR] {e}'
+        print(e)
+        return f'[DEPOSIT_ERROR]'
+
+def test():
+    print(get_highest_deposit_rate())
+
+test()
