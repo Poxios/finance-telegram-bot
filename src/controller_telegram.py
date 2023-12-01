1987288f7215e2387d45b4616014a91cab266d0c
Poxios <poxios0310@gmail.com>
2023-12-01 14:38:35 +0900

    first updated

--- src/controller_telegram.py
+++ src/controller_telegram.py
@@ -1,6 +1,5 @@
-from telegram.ext import CommandHandler
-from telegram.ext import MessageHandler
-from telegram.ext import Updater
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram import Update
 import telegram
 import json
 
@@ -10,25 +9,22 @@ from api_stock import get_full_finance_info_message
 
 
 def get_telegram_bot_token_from_secret():
-    with open('./src/secrets.json', 'r') as file:
+    with open("./src/secrets.json", "r") as file:
         data = json.load(file)
-        return (data['TELEGRAM_BOT_TOKEN'], data['TELEGRAM_BOT_ADMIN_USER_ID'])
+        return (data["TELEGRAM_BOT_TOKEN"], data["TELEGRAM_BOT_ADMIN_USER_ID"])
 
 
 TELEGRAM_BOT_TOKEN = get_telegram_bot_token_from_secret()[0]
 TELEGRAM_ADMIN_USER_ID = get_telegram_bot_token_from_secret()[1]
 
-bot = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
-updater = Updater(token=TELEGRAM_BOT_TOKEN)
-
+app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+bot = telegram.Bot(TELEGRAM_BOT_TOKEN)
 # ! Define Methods
 
 
-def send_telegram_message(userId: int, message: str):
-    bot.sendMessage(chat_id=userId, text=message, parse_mode='HTML')
+async def send_telegram_message(userId: int, message: str):
+    bot.send_message(chat_id=userId, text=message, parse_mode="HTML")
 
-
-dp = updater.dispatcher
 
 # ! Registering Command Handlers
 # ? Manage Stock
@@ -38,20 +34,22 @@ dp = updater.dispatcher
 def command_handler_add_stock_ticker_to_user(update, context):
     user_id = update.effective_chat.id
     if not len(context.args):
-        res_str = 'Please enter the stock ticker.\n'
-        res_str += '<code>/addstock QQQ</code>'
-        context.bot.send_message(
-            chat_id=user_id, text=res_str, parse_mode="HTML")
+        res_str = "Please enter the stock ticker.\n"
+        res_str += "<code>/addstock QQQ</code>"
+        context.bot.send_message(chat_id=user_id, text=res_str, parse_mode="HTML")
         return
     ticker_to_add = context.args[0].upper()
     if controller_sqlite.favorite_stock_add_to_user(user_id, ticker_to_add):
-        context.bot.send_message(chat_id=user_id, text='Add Done')
+        context.bot.send_message(chat_id=user_id, text="Add Done")
     else:
-        context.bot.send_message(chat_id=user_id, text='Add Failed')
+        context.bot.send_message(chat_id=user_id, text="Add Failed")
 
 
-dp.add_handler(CommandHandler('addstock', pass_args=True,
-               callback=command_handler_add_stock_ticker_to_user))
+app.add_handler(
+    CommandHandler(
+        "addstock", pass_args=True, callback=command_handler_add_stock_ticker_to_user
+    )
+)
 
 # $ Remove stock ticker from user stock list
 
@@ -59,13 +57,17 @@ dp.add_handler(CommandHandler('addstock', pass_args=True,
 def command_handler_remove_stock_ticker_to_user(update, context):
     user_id = update.effective_chat.id
     ticker_to_remove = context.args[0].upper()
-    controller_sqlite.favorite_stock_remove_from_user(
-        user_id, ticker_to_remove)
-    context.bot.send_message(chat_id=user_id, text='Remove Done')
+    controller_sqlite.favorite_stock_remove_from_user(user_id, ticker_to_remove)
+    context.bot.send_message(chat_id=user_id, text="Remove Done")
 
 
-dp.add_handler(CommandHandler('removestock', pass_args=True,
-               callback=command_handler_remove_stock_ticker_to_user))
+app.add_handler(
+    CommandHandler(
+        "removestock",
+        pass_args=True,
+        callback=command_handler_remove_stock_ticker_to_user,
+    )
+)
 
 # $ Get my favorite stock list
 
@@ -73,11 +75,14 @@ dp.add_handler(CommandHandler('removestock', pass_args=True,
 def command_handler_get_favorite_stock_list(update, context):
     user_id = update.effective_chat.id
     context.bot.send_message(
-        chat_id=TELEGRAM_ADMIN_USER_ID, text=controller_sqlite.favorite_stock_get_list(user_id))
+        chat_id=TELEGRAM_ADMIN_USER_ID,
+        text=controller_sqlite.favorite_stock_get_list(user_id),
+    )
 
 
-dp.add_handler(CommandHandler('showstocks',
-                              callback=command_handler_get_favorite_stock_list))
+app.add_handler(
+    CommandHandler("showstocks", callback=command_handler_get_favorite_stock_list)
+)
 
 
 # ? Execute Command
@@ -88,10 +93,12 @@ def command_handler_new_user_detected(update, context):
     user_id = update.effective_chat.id
     user_name = update.effective_user.full_name
     context.bot.send_message(
-        chat_id=TELEGRAM_ADMIN_USER_ID, text=f'New user detected: {user_id} / {user_name}')
+        chat_id=TELEGRAM_ADMIN_USER_ID,
+        text=f"New user detected: {user_id} / {user_name}",
+    )
 
 
-dp.add_handler(CommandHandler('start', command_handler_new_user_detected))
+app.add_handler(CommandHandler("start", command_handler_new_user_detected))
 
 # $ New User add (ADMIN ONLY)
 
@@ -103,11 +110,14 @@ def command_handler_new_user_add(update, context):
     user_id_to_add, user_name = context.args
     controller_sqlite.user_add_new_user(user_id_to_add, user_name)
     context.bot.send_message(
-        chat_id=TELEGRAM_ADMIN_USER_ID, text=f'Adding new user done {user_id_to_add} / {user_name}')
+        chat_id=TELEGRAM_ADMIN_USER_ID,
+        text=f"Adding new user done {user_id_to_add} / {user_name}",
+    )
 
 
-dp.add_handler(CommandHandler('adduser', pass_args=True,
-               callback=command_handler_new_user_add))
+app.add_handler(
+    CommandHandler("adduser", pass_args=True, callback=command_handler_new_user_add)
+)
 
 # $ Get User List (ADMIN ONLY)
 
@@ -117,43 +127,44 @@ def command_handler_get_user_list(update, context):
     if str(TELEGRAM_ADMIN_USER_ID) != str(user_id):
         return
     context.bot.send_message(
-        chat_id=TELEGRAM_ADMIN_USER_ID, text=str(controller_sqlite.user_get_list()))
+        chat_id=TELEGRAM_ADMIN_USER_ID, text=str(controller_sqlite.user_get_list())
+    )
 
 
-dp.add_handler(CommandHandler('getuser',
-               callback=command_handler_get_user_list))
+app.add_handler(CommandHandler("getuser", callback=command_handler_get_user_list))
 
 
 # $ Send Command List
 
+
 def command_handler_help(update, context):
-    response_str = ''
-    response_str += '<code>/addstock QQQ    </code> Add QQQ to your favorite stock list.\n'
-    response_str += '<code>/removestock QQQ </code> Remove QQQ from your favorite stock list.\n'
-    response_str += '<code>/showstocks      </code> Show your favorite stocks list.\n'
-    response_str += '<code>/fetchnow        </code> Fetch finance info now.'
+    response_str = ""
+    response_str += (
+        "<code>/addstock QQQ    </code> Add QQQ to your favorite stock list.\n"
+    )
+    response_str += (
+        "<code>/removestock QQQ </code> Remove QQQ from your favorite stock list.\n"
+    )
+    response_str += "<code>/showstocks      </code> Show your favorite stocks list.\n"
+    response_str += "<code>/fetchnow        </code> Fetch finance info now."
     user_id = update.effective_chat.id
-    context.bot.send_message(
-        chat_id=user_id, text=response_str, parse_mode='HTML')
+    context.bot.send_message(chat_id=user_id, text=response_str, parse_mode="HTML")
 
 
-dp.add_handler(CommandHandler(
-    'help', command_handler_help))
+app.add_handler(CommandHandler("help", command_handler_help))
 
 # $ Fetch finance and send it.
 
 
 def command_handler_fetch_now(update, context):
     user_id = update.effective_chat.id
-    context.bot.send_message(
-        chat_id=user_id, text='Fetching finance info..')
+    context.bot.send_message(chat_id=user_id, text="Fetching finance info..")
     full_message = get_full_finance_info_message(user_id)
-    context.bot.send_message(
-        chat_id=user_id, text=full_message, parse_mode='HTML')
+    context.bot.send_message(chat_id=user_id, text=full_message, parse_mode="HTML")
 
 
-dp.add_handler(CommandHandler('fetchnow', command_handler_fetch_now))
+app.add_handler(CommandHandler("fetchnow", command_handler_fetch_now))
 
 
 def start_polling():
-    updater.start_polling()
+    app.run_polling()
