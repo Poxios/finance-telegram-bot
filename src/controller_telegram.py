31cbc1482d68e4c0e5bb9faf647150905515bd3f
Poxios <poxios0310@gmail.com>
2023-12-01 15:56:58 +0900

    cannot handle async

--- src/controller_telegram.py
+++ src/controller_telegram.py
@@ -23,7 +23,7 @@ bot = telegram.Bot(TELEGRAM_BOT_TOKEN)
 
 
 async def send_telegram_message(userId: int, message: str):
-    bot.send_message(chat_id=userId, text=message, parse_mode="HTML")
+    await bot.send_message(chat_id=userId, text=message, parse_mode="HTML")
 
 
 # ! Registering Command Handlers
@@ -31,18 +31,18 @@ async def send_telegram_message(userId: int, message: str):
 # $ Add stock ticker to user stock list
 
 
-def command_handler_add_stock_ticker_to_user(update, context):
+async def command_handler_add_stock_ticker_to_user(update, context):
     user_id = update.effective_chat.id
     if not len(context.args):
         res_str = "Please enter the stock ticker.\n"
         res_str += "<code>/addstock QQQ</code>"
-        context.bot.send_message(chat_id=user_id, text=res_str, parse_mode="HTML")
+        await context.bot.send_message(chat_id=user_id, text=res_str, parse_mode="HTML")
         return
     ticker_to_add = context.args[0].upper()
     if controller_sqlite.favorite_stock_add_to_user(user_id, ticker_to_add):
-        context.bot.send_message(chat_id=user_id, text="Add Done")
+        await context.bot.send_message(chat_id=user_id, text="Add Done")
     else:
-        context.bot.send_message(chat_id=user_id, text="Add Failed")
+        await context.bot.send_message(chat_id=user_id, text="Add Failed")
 
 
 app.add_handler(
@@ -54,11 +54,11 @@ app.add_handler(
 # $ Remove stock ticker from user stock list
 
 
-def command_handler_remove_stock_ticker_to_user(update, context):
+async def command_handler_remove_stock_ticker_to_user(update, context):
     user_id = update.effective_chat.id
     ticker_to_remove = context.args[0].upper()
     controller_sqlite.favorite_stock_remove_from_user(user_id, ticker_to_remove)
-    context.bot.send_message(chat_id=user_id, text="Remove Done")
+    await context.bot.send_message(chat_id=user_id, text="Remove Done")
 
 
 app.add_handler(
@@ -72,9 +72,9 @@ app.add_handler(
 # $ Get my favorite stock list
 
 
-def command_handler_get_favorite_stock_list(update, context):
+async def command_handler_get_favorite_stock_list(update, context):
     user_id = update.effective_chat.id
-    context.bot.send_message(
+    await context.bot.send_message(
         chat_id=TELEGRAM_ADMIN_USER_ID,
         text=controller_sqlite.favorite_stock_get_list(user_id),
     )
@@ -89,10 +89,10 @@ app.add_handler(
 # $ New User Detected
 
 
-def command_handler_new_user_detected(update, context):
+async def command_handler_new_user_detected(update, context):
     user_id = update.effective_chat.id
     user_name = update.effective_user.full_name
-    context.bot.send_message(
+    await context.bot.send_message(
         chat_id=TELEGRAM_ADMIN_USER_ID,
         text=f"New user detected: {user_id} / {user_name}",
     )
@@ -103,13 +103,13 @@ app.add_handler(CommandHandler("start", command_handler_new_user_detected))
 # $ New User add (ADMIN ONLY)
 
 
-def command_handler_new_user_add(update, context):
+async def command_handler_new_user_add(update, context):
     user_id = update.effective_chat.id
     if str(TELEGRAM_ADMIN_USER_ID) != str(user_id):
         return
     user_id_to_add, user_name = context.args
     controller_sqlite.user_add_new_user(user_id_to_add, user_name)
-    context.bot.send_message(
+    await context.bot.send_message(
         chat_id=TELEGRAM_ADMIN_USER_ID,
         text=f"Adding new user done {user_id_to_add} / {user_name}",
     )
@@ -122,11 +122,11 @@ app.add_handler(
 # $ Get User List (ADMIN ONLY)
 
 
-def command_handler_get_user_list(update, context):
+async def command_handler_get_user_list(update, context):
     user_id = update.effective_chat.id
     if str(TELEGRAM_ADMIN_USER_ID) != str(user_id):
         return
-    context.bot.send_message(
+    await context.bot.send_message(
         chat_id=TELEGRAM_ADMIN_USER_ID, text=str(controller_sqlite.user_get_list())
     )
 
@@ -137,7 +137,7 @@ app.add_handler(CommandHandler("getuser", callback=command_handler_get_user_list
 # $ Send Command List
 
 
-def command_handler_help(update, context):
+async def command_handler_help(update, context):
     response_str = ""
     response_str += (
         "<code>/addstock QQQ    </code> Add QQQ to your favorite stock list.\n"
@@ -148,7 +148,7 @@ def command_handler_help(update, context):
     response_str += "<code>/showstocks      </code> Show your favorite stocks list.\n"
     response_str += "<code>/fetchnow        </code> Fetch finance info now."
     user_id = update.effective_chat.id
-    context.bot.send_message(chat_id=user_id, text=response_str, parse_mode="HTML")
+    await context.bot.send_message(chat_id=user_id, text=response_str, parse_mode="HTML")
 
 
 app.add_handler(CommandHandler("help", command_handler_help))
@@ -156,16 +156,16 @@ app.add_handler(CommandHandler("help", command_handler_help))
 # $ Fetch finance and send it.
 
 
-def command_handler_fetch_now(update, context):
+async def command_handler_fetch_now(update, context):
     user_id = update.effective_chat.id
-    context.bot.send_message(chat_id=user_id, text="Fetching finance info..")
+    await context.bot.send_message(chat_id=user_id, text="Fetching finance info..")
     full_message = get_full_finance_info_message(user_id)
-    context.bot.send_message(chat_id=user_id, text=full_message, parse_mode="HTML")
+    await context.bot.send_message(chat_id=user_id, text=full_message, parse_mode="HTML")
 
 
 app.add_handler(CommandHandler("fetchnow", command_handler_fetch_now))
 
 
-def start_polling():
+async def start_polling():
     print('[SYSTEM] Start telegram polling...')
-    app.run_polling()
+    await app.run_polling()
